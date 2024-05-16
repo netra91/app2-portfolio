c2ed6013c8a9536be9b75b814860044d87036e80
netra91 <netrapb06@gmail.com>
2024-05-16 12:37:33 +0530

    completed

--- pages/contact_us.py
+++ pages/contact_us.py
@@ -7,9 +7,11 @@ with st.form(key="email_forms"):
     user_email = st.text_input("your email address")
     raw_message = st.text_area("enter your message")
     message = f"""\
+    
     subject: new email from {user_email}
     
     from:{user_email}
+    
     {raw_message}
     """
     button = st.form_submit_button("submit")
